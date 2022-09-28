425d180b617a31558f50650a029ba3fcd46c10f7
Gonzalo <zapozapico@gmail.com>
2022-09-27 23:36:11 -0300

    Add route get product/byCategory

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,8 +1,7 @@
 const { Router } = require('express');
-const { Product } = require('../db');
 const { Op } = require("sequelize")
 const axios = require("axios");
-const { User, Cart, Categories, Color, Image, Orders, Products, Reviews, conn} = require('../db') 
+const { User, Cart, Category, Color, Image, Orders, Product, Reviews, conn} = require('../db') 
 
 
 const router = Router();
@@ -71,13 +70,30 @@ router.get("/all", async (req, res) => {
     } catch (err) {
         res.status(500).send({error: err.message})
     }
+});
+
+router.get("/byCategory/:category", async (req, res) => {
+    const { category } = req.params;
+    try {
+        const products = await Product.findAll({include: {
+            model: Category,
+            required: true,
+            where: {
+                name: category
+            }
+        }});
+        res.send(products);
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+
 })
 
 
 // Crear ruta para crear/agregar Producto listo
-//     Crear ruta para Modificar Producto
+//     Crear ruta para Modificar Producto listo
 //     Crear ruta para ocultar producto listo
-//     Crear ruta que devuelva todos los productos
+//     Crear ruta que devuelva todos los productos listo
 //     Crear Ruta que devuelva los productos de X categoria
 //     Crear ruta de producto individual, pasado un ID que retorne un producto con sus detalles
 
