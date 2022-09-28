d863292c1c0e974774cfb3dcf8af3fbcbbf21c74
Gonzalo <zapozapico@gmail.com>
2022-09-28 16:25:19 -0300

    Fix route review/add

--- src/models/Reviews.js
+++ src/models/Reviews.js
@@ -4,11 +4,11 @@ module.exports = (sequelize) => {
     sequelize.define("review", {
         description: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         },
         stars: {
             type: DataTypes.ENUM("1","2","3","4","5"),
-            allowNull: false
+            allowNull: true
         }
     }, {
         timestamps: false
