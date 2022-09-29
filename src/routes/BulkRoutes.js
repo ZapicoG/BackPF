12ac37ce228e037fbaef930670d0488e31e81e2b
Gonzalo <zapozapico@gmail.com>
2022-09-28 22:19:11 -0300

    Added bulk/products and bulk/categories routes

--- src/routes/BulkRoutes.js
+++ src/routes/BulkRoutes.js
@@ -11,10 +11,36 @@ router.post("/user", async (req, res) => {
     const { users } = req.body;
 
     try {
-        console.log(users)
+        // console.log(users)
         await User.bulkCreate(users)
         res.send("Users created")
     } catch (err) {
         res.status(500).send({error: err.message})
     }
-})
+});
+
+
+router.post("/products", async (req, res) => {
+    const { products } = req.body;
+
+    try {
+        // console.log(users)
+        await User.bulkCreate(products)
+        res.send("Products created")
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+});
+
+
+router.post("/categories", async (req, res) => {
+    const { categories } = req.body;
+
+    try {
+        console.log(categories)
+        await User.bulkCreate(categories)
+        res.send("Categories created")
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+});
